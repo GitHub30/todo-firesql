8ea82e2c9fd0cafbc84c2d17edd3fd069a39ad59
Tomofumi Inoue <funaox@gmail.com>
2019-07-22 14:27:26 +0000

    Update

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -1,88 +1,4 @@
-import io from 'socket.io-client'
-
-class FireSQL {
-  constructor(url) {
-    // eslint-disable-next-line no-console
-    console.log('initialize')
-    this.socket = io(url)
-  }
-
-  on(table, callback) {
-    this.socket.emit('_enter_room', table)
-    this.socket.on(table, callback)
-  }
-
-  query(sql) {
-    // eslint-disable-next-line no-console
-    console.log(sql)
-    this._clearSQL()
-    // eslint-disable-next-line prettier/prettier
-    return new Promise(resolve => this.socket.emit('_query', sql, resolve))
-  }
-
-  _clearSQL() {
-    this.tableName = ''
-    this.whereString = ''
-  }
-
-  table(tableName) {
-    this.tableName = tableName
-    return this
-  }
-
-  where(...args) {
-    if (args.length === 2) args = [args[0], '=', args[1]]
-    const condition = `${args[0]}${args[1]}${this._escape(args[2])}`
-    if (this.whereString) {
-      this.whereString += ` AND ${condition}`
-    } else {
-      this.whereString = ` WHERE ${condition}`
-    }
-    return this
-  }
-
-  select(...columns) {
-    const columnsString = columns.length ? columns.join(', ') : '*'
-    const sql = `SELECT ${columnsString} FROM ${this.tableName}${this.whereString}`
-    // eslint-disable-next-line prettier/prettier
-    return new Promise(resolve => this.query(sql).then(resolve))
-  }
-
-  insert(...valuesArray) {
-    const columnsString = Object.keys(valuesArray[0]).join(', ')
-    const valuesArrayString = valuesArray
-      // eslint-disable-next-line prettier/prettier
-      .map(values => `(${Object.values(values).map(this._escape).join(', ')})`)
-      .join(', ')
-    const sql = `INSERT INTO ${this.tableName} (${columnsString}) VALUES ${valuesArrayString}`
-    // eslint-disable-next-line prettier/prettier
-    return new Promise(resolve => this.query(sql).then(resolve))
-  }
-
-  update(values) {
-    const valuesString = Object.entries(values)
-      .map(([column, value]) => `${column}=${this._escape(value)}`)
-      .join(', ')
-    const sql = `UPDATE ${this.tableName} SET ${valuesString}${this.whereString}`
-    // eslint-disable-next-line prettier/prettier
-    return new Promise(resolve => this.query(sql).then(resolve))
-  }
-
-  delete() {
-    const sql = `DELETE FROM ${this.tableName}${this.whereString}`
-    // eslint-disable-next-line prettier/prettier
-    return new Promise(resolve => this.query(sql).then(resolve))
-  }
-
-  _escape(data) {
-    switch (typeof data) {
-      case 'string':
-        return `'${data.replace(/'/g, "''").replace(/\\/g, '\\\\')}'`
-      default:
-        return data
-    }
-  }
-}
+import { FireSQL } from 'firesql.js'
 
 export default ({ store }) => {
   const firesql = new FireSQL('http://localhost:8080')
@@ -93,8 +9,8 @@ export default ({ store }) => {
 
   window.syncState = ({ table, pkColumnName, pk, jsonColumnName }) => {
     let ownEvent = false
-    firesql.socket.off(event)
-    if (event) event = table + '/' + pk
+    if (event) firesql.socket.off(event)
+    event = table + '/' + pk
     firesql.on(event, (row, type) => {
       if (ownEvent) {
         ownEvent = false
